d399e171fb4e89e0fad9b8a3025eeb2bd817eaad
Mihirsinh Chauhan <112346682+MihirsinhChauhan@users.noreply.github.com>
2025-08-22 21:44:09 +0530

    task and category storage

--- code/Todo-app/server/storage/memory.go
+++ code/Todo-app/server/storage/memory.go
@@ -1,1 +1,30 @@
 package storage
+
+import "todo-backend/models"
+
+type CategoryStorage struct {
+	categories []models.Category
+}
+
+type TaskStorage struct {
+	tasks []models.Task
+}
+func NewTaskStorage() *TaskStorage {
+	return &TaskStorage{
+		tasks: []models.Task{},
+	}
+}
+
+func (ts *TaskStorage) AddTask(task models.Task) {
+	ts.tasks = append(ts.tasks, task)
+}
+
+func NewCategoryStorage() *CategoryStorage {
+	return &CategoryStorage{
+		categories: []models.Category{},
+	}
+}
+
+func (cs *CategoryStorage) AddCategory(category models.Category) {
+	cs.categories = append(cs.categories, category)
+}
--- code/Todo-app/server/storage/memory_test.go
+++ code/Todo-app/server/storage/memory_test.go
@@ -1,1 +1,23 @@
 package storage
+
+import (
+	"todo-backend/models"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewCategoryStorage(t *testing.T) {
+	category := models.NewCategory(1, "Test Category", "2023-10-01T10:00:00Z")
+	storage := NewCategoryStorage()
+	storage.AddCategory(category)
+	assert.Equal(t, 1, len(storage.categories))
+	assert.Equal(t, "Test Category", storage.categories[0].Name)
+}
+
+func TestNewTaskStorage(t *testing.T) {
+	task := models.NewTask(1, "Test Task", "This is a test task", "HIGH", nil, false, "2023-10-01T10:00:00Z")
+	storage := NewTaskStorage()
+	storage.AddTask(task)
+	assert.Equal(t, 1, len(storage.tasks))
+	assert.Equal(t, "Test Task", storage.tasks[0].Title)
+}	
