07d1e17a6aa0af90ca08b8d5ce3a60a2d0853713
Mihirsinh Chauhan <112346682+MihirsinhChauhan@users.noreply.github.com>
2025-08-22 21:28:39 +0530

    category models

--- code/Todo-app/server/models/category_test.go
+++ code/Todo-app/server/models/category_test.go
@@ -1,1 +1,13 @@
 package models
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewCategory(t *testing.T) {
+
+	category := NewCategory(1,"Learn Go", "2023-10-01T10:00:00Z")
+	assert.Equal(t, "Learn Go", category.Name)
+	assert.NotEmpty(t, category.CreatedAt)
+}
